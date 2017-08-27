52a2f118a9acef519ab711651b8b4dd0e3c4fe55
Daniele Morosinotto <d.morosinotto@icloud.com>
2017-08-27 10:47:06 +0200

    step6 - add selectHero logic + template - show Error binding undefined

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,13 +29,19 @@ const HEROES: Hero[] = [
       <span class="badge">{{hero.id}}</span> {{hero.name}}
     </li>
   </ul>
+  <h2>{{selectedHero.name}} details!</h2>
+  <div><label>id: </label>{{selectedHero.id}}</div>
+  <div>
+      <label>name: </label>
+      <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+  </div>
   `
 })
 export class AppComponent {
   title = 'Tour of Heroes';
   heroes = HEROES;
-  
+  selectedHero: Hero;
   onSelect(hero: Hero): void {
-    console.log(hero);
+    this.selectedHero = hero;
   }
 }
