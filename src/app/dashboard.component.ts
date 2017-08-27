12797e206fefeb6dd1837ad05d16f956b015b5be
Daniele Morosinotto <d.morosinotto@icloud.com>
2017-08-27 14:35:35 +0200

    step17 - modify dashboard template to show first 4th heros - read sharing data with HeroService

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,3 +1,5 @@
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class DashboardComponent implements OnInit {
+  heroes: Hero[];
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.heroService.getHeroes()
+      .then(heros => this.heroes = heros.slice(1,5))
   }
 
 }
